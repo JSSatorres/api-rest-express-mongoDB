4a17f5c8eb3f1e1ef07636b58a3c9b1e879b8d28
jssatorres <juansataz.dev@gmail.com>
2022-12-06 11:38:16 +0100

    Feat (Link) add link routes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express  from "express"
 import authRoutes from './routes/auth.routes.js'
+import linkRoutes from './routes/link.routes.js'
 import cookieParser from "cookie-parser";
 
 const app = express()
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(authRoutes);
+app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/link", linkRoutes);
 
 export default app
